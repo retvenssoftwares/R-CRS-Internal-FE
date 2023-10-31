3deee8e6b265535ea50718523baba1c3b1e77668
major99 <amandecembersharma@gmail.com>
2023-10-31 15:56:33 +0530

    updated ghaction

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:"http://localhost:8000/api",
+  baseURL:"https://admin.retvenscrm.com/api",
   validateStatus: (status) => status < 500,
 });
